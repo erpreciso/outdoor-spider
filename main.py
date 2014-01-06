8c5ebfc1c2a7deb1d385d7eeea525054ecb798b6
erpreciso <trepreciso@gmail.com>
2014-01-06 10:28:45 +0100

    Project start: uploaded all work done so far

---
+++ main.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import jinja2
+import os
+import json, urllib
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+		autoescape = True)
+
+class MainHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class MainPage(MainHandler):
+    def get(self):
+        self.render("main.html")
+
+    def post(self):
+        #current_position = self.request.get("current_position")
+        #self.write("Hai inserito: " + current_position)
+		pass
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage)
+], debug=True)
