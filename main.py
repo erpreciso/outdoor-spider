0d99a7f9fa0a503374de8353b9a451896d6eb69a
erpreciso <trepreciso@gmail.com>
2014-01-19 12:14:47 +0100

    started implementing geocoding, but now let's come back and close the distance spider

--- main.py
+++ main.py
@@ -4,16 +4,6 @@
 #    OUTDOOR-SPIDER app
 #
 # TESTS AND NOTES AREA
-JSO = """
-{
-    u'originAddresses': [u'Milan, Italy'],
-    u'destinationAddresses': [u'Trento, Italy', u'Genoa, Italy'], 
-    u'rows':[
-        {u'elements': [
-            {u'status': u'OK', u'duration': {u'text': u'2 hours 23 mins', u'value': 8558}, u'distance': {u'text': u'223 km', u'value': 223165}}, 
-            {u'status': u'OK', u'duration': {u'text': u'1 hour 46 mins', u'value': 6346}, u'distance': {u'text': u'147 km', u'value': 146658}}
-        ]}]
-}"""
 
 TODO = """
 
@@ -50,6 +40,15 @@ from google.appengine.ext.webapp import blobstore_handlers
 
 mdict = {"distance":{}, "geocoding": {}}
 
+mdict_test = {'geocoding': 
+                {
+                (43.2128473, -75.45573030000003): {u'geometry': {u'viewport': {u'ea': {u'b': 43.281523, u'd': 43.168892}, u'fa': {u'b': -75.66701699999999, u'd': -75.340914}}, u'location': {u'e': -75.45573030000003, u'd': 43.2128473}, u'location_type': u'APPROXIMATE', u'bounds': {u'ea': {u'b': 43.281523, u'd': 43.168892}, u'fa': {u'b': -75.66701699999999, u'd': -75.340914}}}, u'address_components': [{u'short_name': u'Rome', u'long_name': u'Rome', u'types': [u'locality', u'political']}, {u'short_name': u'Oneida', u'long_name': u'Oneida', u'types': [u'administrative_area_level_2', u'political']}, {u'short_name': u'NY', u'long_name': u'New York', u'types': [u'administrative_area_level_1', u'political']}, {u'short_name': u'US', u'long_name': u'United States', u'types': [u'country', u'political']}], u'formatted_address': u'Rome, NY, USA', u'types': [u'locality', u'political']}, 
+                (26.4061111, -99.00555559999998): {u'geometry': {u'viewport': {u'ea': {u'b': 26.4516749, u'd': 26.3915599}, u'fa': {u'b': -99.03279299999997, u'd': -98.97646989999998}}, u'location': {u'e': -99.00555559999998, u'd': 26.4061111}, u'location_type': u'APPROXIMATE', u'bounds': {u'ea': {u'b': 26.4516749, u'd': 26.3915599}, u'fa': {u'b': -99.03279299999997, u'd': -98.97646989999998}}}, u'address_components': [{u'short_name': u'Roma', u'long_name': u'Roma', u'types': [u'locality', u'political']}, {u'short_name': u'Starr', u'long_name': u'Starr', u'types': [u'administrative_area_level_2', u'political']}, {u'short_name': u'TX', u'long_name': u'Texas', u'types': [u'administrative_area_level_1', u'political']}, {u'short_name': u'US', u'long_name': u'United States', u'types': [u'country', u'political']}], u'formatted_address': u'Roma, TX, USA', u'types': [u'locality', u'political']}, 
+                (41.4393617, 12.786070800000061): {u'geometry': {u'viewport': {u'ea': {u'b': 41.4462203, u'd': 41.4317114}, u'fa': {u'b': 12.78071030000001, u'd': 12.797103500000048}}, u'location': {u'e': 12.786070800000061, u'd': 41.4393617}, u'location_type': u'APPROXIMATE', u'bounds': {u'ea': {u'b': 41.4462203, u'd': 41.4317114}, u'fa': {u'b': 12.78071030000001, u'd': 12.797103500000048}}}, u'address_components': [{u'short_name': u'Rome', u'long_name': u'Rome', u'types': [u'locality', u'political']}, {u'short_name': u'Latina', u'long_name': u'Latina', u'types': [u'administrative_area_level_3', u'political']}, {u'short_name': u'LT', u'long_name': u'Latina', u'types': [u'administrative_area_level_2', u'political']}, {u'short_name': u'Lazio', u'long_name': u'Lazio', u'types': [u'administrative_area_level_1', u'political']}, {u'short_name': u'IT', u'long_name': u'Italy', u'types': [u'country', u'political']}, {u'short_name': u'04100', u'long_name': u'04100', u'types': [u'postal_code']}], u'formatted_address': u'04100 Rome, Italy', u'types': [u'locality', u'political']}, 
+                (41.8929163, 12.482519899999943): {u'geometry': {u'viewport': {u'ea': {u'b': 42.0505462, u'd': 41.7695961}, u'fa': {u'b': 12.341707000000042, u'd': 12.73028880000004}}, u'location': {u'e': 12.482519899999943, u'd': 41.8929163}, u'location_type': u'APPROXIMATE', u'bounds': {u'ea': {u'b': 42.0505462, u'd': 41.7695961}, u'fa': {u'b': 12.341707000000042, u'd': 12.73028880000004}}}, u'address_components': [{u'short_name': u'Rome', u'long_name': u'Rome', u'types': [u'locality', u'political']}, {u'short_name': u'Rome', u'long_name': u'Rome', u'types': [u'administrative_area_level_3', u'political']}, {u'short_name': u'RM', u'long_name': u'Rome', u'types': [u'administrative_area_level_2', u'political']}, {u'short_name': u'Lazio', u'long_name': u'Lazio', u'types': [u'administrative_area_level_1', u'political']}, {u'short_name': u'IT', u'long_name': u'Italy', u'types': [u'country', u'political']}], u'formatted_address': u'Rome, Italy', u'types': [u'locality', u'political']}}, 
+            'distance': {}
+            }
+
 def put_origin_in_dict(origin):
     global mdict
     if origin not in get_origin_list():
@@ -63,8 +62,11 @@ def put_destination_in_dict(origin, destination, travel_info):
 
 def get_origin_list():
     """return list of all origins."""
-    global mdict
     return mdict["distance"].keys()
+
+def get_location_list():
+    """return list of all locations."""
+    return mdict["geocoding"].keys()
 
 def get_destination_list_from(origin):
     """return list all destination from given origin."""
@@ -89,7 +91,16 @@ def get_destinations_from_origin_given_distance(origin, distance, tolerance):
     # return only those in scope
     mn, mx = distance - tolerance, distance + tolerance
     return [(dist[d], d) for d in dist.keys() if d > mn and d < mx] 
-    
+
+def put_geocoding_results(results):
+    assert type(results) == list
+    global mdict
+    for result in results:
+        key = result["geometry"]["location"]
+        (lat, lng) = key["d"], key["e"]
+        if (lat, lng) not in get_location_list():
+            mdict["geocoding"][(lat, lng)] = result
+    logging.info(mdict)
 # ----------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------
@@ -162,7 +173,8 @@ class PostGeocoding(MainHandler):
     def post(self):
         #~ load json object coming from js ajax
         js = json.loads(self.request.body)
-        logging.info(str(js))
+        #~ logging.info(str(js))
+        put_geocoding_results(js)
 
 class PostDistance(MainHandler):
     def post(self):
